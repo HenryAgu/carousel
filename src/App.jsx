eb704d902cfc6ca7979985795130e71f13ed2791
Henry Agu <aguhenrychuks@gmail.com>
2026-02-13 23:09:29 +0100

    feat: animations and dynamic images and texts

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import { motion } from "motion/react";
+import { AnimatePresence, motion } from "motion/react";
 import "./App.css";
+import { useState } from "react";
 
-const text = "Italy";
+const carouselList = [
+  {
+    image: "/images/italy.webp",
+    name: "Italy",
+  },
+  {
+    image: "/images/happiness.webp",
+    name: "Happiness",
+  },
+  {
+    image: "/images/brazil.webp",
+    name: "Brazil",
+  },
+  {
+    image: "/images/car.webp",
+    name: "Car",
+  },
+];
 
 const container = {
   hidden: { opacity: 0, y: -50 },
@@ -9,6 +27,7 @@ const container = {
     opacity: 1,
     y: 0,
     transition: {
+      type: "spring",
       duration: 0.8,
       ease: "easeOut",
       staggerChildren: 0.08,
@@ -33,39 +52,72 @@ const child = {
   },
 };
 
+const imageVariants = {
+  hidden: { opacity: 0, y: -50 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.8,
+      ease: "easeOut",
+    },
+  },
+  exit: {
+    opacity: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
+
 function App() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % carouselList.length);
+  }
+
+  const handlePrevious = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + carouselList.length) % carouselList.length);
+  }
+
+  const currentItem = carouselList[currentIndex];
+
   return (
     <main className="relative h-screen w-screen p-4 overflow-hidden flex flex-col">
-      <div className="p-4">
-        <p>Close Project</p>
-      </div>
-      <div className="flex justify-between gap-4 flex-1 min-h-10 py-4">
-        <motion.p
-          className="font-dancing-script font-bold text-4xl sm:text-5xl md:text-6xl lg:text-7xl whitespace-nowrap"
-          variants={container}
-          initial="hidden"
-          animate="visible"
-        >
-          {text.split("").map((letter, index) => (
-            <motion.span key={index} variants={child}>
-              {letter}
-            </motion.span>
-          ))}
-        </motion.p>
-        <motion.div variants={container} initial="hidden" animate="visible">
-          <img
-            src="/images/italy.webp"
+      <div className="flex lg:flex-row flex-col justify-center lg:justify-between gap-4 flex-1 min-h-10 py-4">
+        <AnimatePresence mode="wait">
+          <motion.p
+            className="font-dancing-script font-bold text-4xl sm:text-5xl md:text-6xl lg:text-7xl whitespace-nowrap"
+            key={currentItem.name}
+            variants={container}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+          >
+            {currentItem.name.split("").map((letter, index) => (
+              <motion.span key={index} variants={child}>
+                {letter}
+              </motion.span>
+            ))}
+          </motion.p>
+        </AnimatePresence>
+        <AnimatePresence mode="wait">
+          <motion.img
+          key={currentItem.name}
+            src={currentItem.image}
             alt="image"
             className="max-w-full max-h-full w-auto h-auto object-contain"
+            initial="hidden" variants={imageVariants} animate="visible" exit="exit"
           />
-        </motion.div>
+        </AnimatePresence>
       </div>
       <div className="flex justify-between items-center p-4">
-        <button className="uppercase font-semibold text-xs sm:text-sm md:text-base pb-0 cursor-pointer tracking-normal hover:tracking-wider transition-all duration-300 ease-in">
+        <button className="uppercase font-semibold text-xs sm:text-sm md:text-base pb-0 cursor-pointer tracking-normal hover:tracking-wider transition-all duration-300 ease-in" onClick={handlePrevious}>
           <span className="lg:block hidden">Previous Project</span>
           <span className="block lg:hidden">Previous</span>
         </button>
-        <button className="uppercase font-semibold text-xs sm:text-sm md:text-base pb-0 cursor-pointer tracking-normal hover:tracking-wider transition-all duration-300 ease-in">
+        <button className="uppercase font-semibold text-xs sm:text-sm md:text-base pb-0 cursor-pointer tracking-normal hover:tracking-wider transition-all duration-300 ease-in" onClick={handleNext}>
           <span className="lg:block hidden">Next Project</span>
           <span className="block lg:hidden">Next</span>
         </button>
